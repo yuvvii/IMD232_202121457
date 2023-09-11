e11da60cf556fa8a6071a09e7482a7c91f4b50a4
yuvvii <143991614+yuvvii@users.noreply.github.com>
2023-09-11 17:42:06 +0900

    week2

---
+++ src/w2/sketches/sketch2/script/alertPromptConfirm.js
@@ -0,0 +1,3 @@
+alert('저를 살려주세요ㅜㅜ~');
+let userTyped = prompt('이름을 불러바', '율리야');
+confirm('제발 ' + userTyped + ' 살려줘');
---
+++ src/w2/sketches/sketch4/script/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  circle(mouseX, mouseY, 100);
+}
