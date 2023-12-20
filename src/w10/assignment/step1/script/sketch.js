62d1868733a56a363dc46d08af31bac7061d2f0a
yuvvii <143991614+yuvvii@users.noreply.github.com>
2023-12-20 16:14:52 +0900

    1

--- src/w10/assignment/step1/script/sketch.js
+++ src/w10/assignment/step1/script/sketch.js
@@ -1,10 +1,9 @@
-class GameOfLife {
+class Game {
   constructor() {
     this.cells = [];
-    this.rowNum = 50;
-    this.colNum = 50;
+    this.rowNum = 80;
+    this.colNum = 80;
 
-    // 셀 생성 및 배치
     for (let row = 0; row < this.rowNum; row++) {
       for (let col = 0; col < this.colNum; col++) {
         const x = (width / this.colNum) * col;
@@ -18,7 +17,7 @@ class GameOfLife {
         this.cells.push(newCell);
       }
     }
-    // 이웃 셀 설정
+
     for (let row = 0; row < this.rowNum; row++) {
       for (let col = 0; col < this.colNum; col++) {
         const neighborsIdx = [
@@ -32,7 +31,6 @@ class GameOfLife {
           this.getIdx(row, col - 1),
         ];
 
-        // 가장자리 처리
         if (col === 0) {
           neighborsIdx[0] = -1;
           neighborsIdx[6] = -1;
@@ -62,31 +60,29 @@ class GameOfLife {
         this.cells[idx].setNeighbors(neighbors);
       }
     }
-    // 초기 상태 무작위 설정
+
     randomSeed(1);
     this.cells.forEach((each) => {
-      const randomState = floor(random(3)); // 0: 바위, 1: 보, 2: 가위
+      const randomState = floor(random(3));
       each.state = randomState;
     });
   }
-  // 행렬 인덱스 반환
+
   getIdx(row, col) {
     return row * this.colNum + col;
   }
 
-  // 그리기 함수
   draw() {
-    background('lightsteelblue');
+    background('pink');
 
-    // 다음 상태 계산
     this.cells.forEach((each) => {
       each.calcNextState();
     });
-    // 상태 업데이트
+
     this.cells.forEach((each) => {
       each.update();
     });
-    // 화면에 표시
+
     this.cells.forEach((each) => {
       each.display(mouseX, mouseY);
     });
@@ -95,31 +91,27 @@ class GameOfLife {
 
 class Cell {
   constructor(x, y, w, h, isClickable = true) {
-    // 생성자
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.isClickable = isClickable;
-    this.state = 0; // 0: 바위, 1: 보, 2: 가위
+    this.state = 0;
     this.nextState = this.state;
     this.neighbors = [];
   }
 
-  // 이웃 셀 설정
   setNeighbors(neighbors) {
     this.neighbors = neighbors;
   }
 
-  // 다음 상태 계산
   calcNextState() {
-    const winningStates = [2, 0, 1]; // 바위는 가위 이김, 가위는 보 이김, 보는 바위 이김
+    const winningStates = [2, 0, 1];
     const winningNeighbors = this.neighbors.filter(
       (eachNeighbor) =>
         eachNeighbor && winningStates[eachNeighbor.state] === this.state
     );
 
-    // 이기는 이웃이 2개 이하이면 현재 상태 유지, 그 이상이면 첫 번째 이웃의 상태로 변경
     if (winningNeighbors.length <= 2) {
       this.nextState = this.state;
     } else {
@@ -128,27 +120,23 @@ class Cell {
     }
   }
 
-  // 상태 업데이트
   update() {
     this.state = this.nextState;
   }
 
-  // 마우스 호버 여부 체크
   isHover(mx, my) {
     return (
       this.x < mx && this.x + this.w > mx && this.y < my && this.y + this.h > my
     );
   }
 
-  // 상태 토글
   toggleState(mx, my) {
     if (!this.isClickable) return false;
     if (!this.isHover(mx, my)) return false;
-    this.state = (this.state + 1) % 3; // 바위, 보, 가위 순환
+    this.state = (this.state + 1) % 3;
     return true;
   }
 
-  // 화면에 표시
   display(mx, my) {
     push();
     translate(this.x, this.y);
@@ -157,9 +145,9 @@ class Cell {
       this.state === 0
         ? color('pink')
         : this.state === 1
-        ? color('yellow')
-        : color('red')
-    ); // 바위, 보, 가위 색상
+        ? color('beige')
+        : color('lime')
+    );
     rect(0, 0, this.w, this.h);
     pop();
   }
@@ -168,7 +156,7 @@ class Cell {
 let game;
 function setup() {
   setCanvasContainer('canvas', 1, 1, true);
-  game = new GameOfLife();
+  game = new Game();
 }
 function draw() {
   game.draw();
